16d6a8e9787b975541ab9abe4631c686ade22823
sidneisilvestre <sidsilvestrenei@gmail.com>
2020-01-04 22:23:44 -0200

    teste

---
+++ relatorio.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+        <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/style.css" />
+    <title>Funerária Só Falta Você - Relatório Financeiro </title>
+  </head>
+  <body>
+  <h1 id="conteudo">Relatório Financeiro </h1>
+<p>Veja abaixo o relatório da movimentação financeira da sua empresa. </p>
+<ul>
+<?php 
+$cx = new mysqli ("localhost","root","","funeraria");
+$eventos= $cx->query ("select * from eventos order by nome");
+/*busca os dados no banco */
+while ($evento = $eventos->fetch_assoc()){
+echo "<li>$evento[nome] - $evento[valor]</li>";
+}
+?>
+</ul>
+
+
+
+  
+  </body>
+</html>
--- topo.php
+++ topo.php
@@ -7,6 +7,7 @@ echo "Aqui ficará o logo"
 <a href="cadastro_produtos.php">Cadastro de Produtos </a>
 <a href="cadastro_planos.php">Cadastro de Planos </a>
 <a href="cadastro_atendimentos.php">Cadastro de Atendimentos </a>
+<a href="relatorio.php">Relatório Financeiro </a>
 <form role="search">
 <label for="p">busque no site</label>
 <input type="text" id="p" name="p" />
