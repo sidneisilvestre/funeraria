1ade2ad513ac557dc76d86b0c5ad2df6ad32b5b3
Daniel Ribeiro <54645074+danielrso@users.noreply.github.com>
2020-01-06 22:47:48 -0300

    Add files via upload

--- produto-cadastro.php
+++ produto-cadastro.php
@@ -2,6 +2,6 @@
 /*comando para logar no banco */
 $cx = new mysqli ("localhost","root","","funeraria");
 /*Método que irá chamar as querys */
-$cx->query("insert into produtos (nome) values('$_POST[produto]')");
+$cx->query("insert into produtos (nome,preco) values('$_POST[produto]','$_POST[preco]')");
 /*função para mandar uma metatag para o navegador */
 header("Location: cadastro_produtos.php");
