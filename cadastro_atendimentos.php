86493f33b2159f2f3383cab41abb4aa5cc53a96f
andersonmogta <54689248+andersonmogta@users.noreply.github.com>
2020-01-12 21:29:41 -0300

    Add files via upload

--- cadastro_atendimentos.php
+++ cadastro_atendimentos.php
@@ -11,16 +11,24 @@
   <body>
   <h1 id="conteudo">Cadastro dos eventos atendidos</h1>
 <form action="eventos_cadastro.php" method="post">
-<label for="mat">Selecione um Evento cadastrado ou adicione um novo</label>
-<select name="mat" id="mat"/>
+<label for ="ev">Selecione um Evento cadastrado para atendimento</label>
+<select name="ev" id="ev"/>
 <?php 
 $cx = new mysqli ("localhost","root","","funeraria");
 $eventos= $cx->query ("select * from eventos order by nome");
 while ($evento = $eventos->fetch_assoc()){
-echo "<option>$evento[nome]</option>";
+echo "<option>$evento[nome]</option value='$evento[id]'>";
 }
 ?>
 </select>
-
+<label for="km!> informe a kilometragem percorrida.</label>
+<imput type="number"> name="km"> id="km";
+<label for="l"> informe quantos litros  de combustível foi gasto.</label>
+<imput type="number"> name="l"> id="l;
+<label for="desp"> informe se ouve despeza com mecânico</label>
+<imput type="text"> name="desp"> id="desp";
+<label for="out"> acrescente outras despezas se houver</label>
+imput type="text"> name="out"> id="out";
+</for>
 </body>
 </html>
