06c542067b03c7f45e99533785475ed496628efb
danielrso <danielrso.iti@gmail.com>
2020-01-17 18:57:15 -0300

    melhoria forulário atendimento, ajuste de sugestão de banco

--- cadastro_atendimentos.php
+++ cadastro_atendimentos.php
@@ -10,35 +10,72 @@
   </head>
   <body>
   <h1 id="conteudo">Cadastro de atendimentos</h1>
-  <h2>Próximos Atendimentos a serem atendidos </h2>
+  <h2>Próximas demandas a serem atendidas </h2>
 <form action="atendimentos_cadastro.php" method="post">
+
+<div class="form-group">
 <label for ="ev">Selecione um Evento cadastrado para atendimento</label>
 <select name="ev" id="ev" autofocus />
-<?php '
+<?php
 $cx = new mysqli ("localhost","root","","funeraria");
 $eventos= $cx->query ("select * from eventos order by nome");
 while ($evento = $eventos->fetch_assoc()){
-echo "<option value='evento[$evento[id]]'>$evento[nome]</option>";
+echo "<option value='evento[id]'>$evento[nome]</option>";
 }
 ?>
 </select>
+</div>
+
+<div class="form-group">
 <label for="km">Informe a Quilometragem percorrida </label>
-<input type="number" name="km" id="km" />
-<label for="desp">Informe a despesa total  </label>
-<input type="number" name="despesa" id="desp" />
+<input type="number" name="km" id="km" class="form-control" />
+</div>
+
+<div class="form-group">
+<label for="ali">Inclua o total de despesas com alimentação</label>
+<input name="ali" id="ali" type="number" class="form-control" />
+</div>
+
+<div class="form-group">
+<label for="ped">Inclua o total de despesas com pedágio </label>
+<input name="ped" id="ped" type="number" class="form-control" />
+</div>
+
+
+<div class="form-group">
+<label for="combu"> Inclua o total de despesas com combustível </label>
+<input type="number" name="combu" id="combu" class="form-control" />
+</div>
+
+<div class="form-group">
+<label for="mec"> Inclua o total de despesas com oficina mecânica </label>
+<input type="number" name="mec" id="mec" class="form-control" />
+</div>
+
+
+<div class="form-group">
 <label for="obs"> Informe os demais ítens ou serviços que foram adquiridos durante este atendimento </label>
-<input type="text" name="obs" id="obs" />
-<button type="submit" id="b" class="btn btn-primary">Cadastrar </button>
+<input type="text" name="obs" id="obs" class="form-control" />
+</div>
+<div class="form-group">
+<fieldset>
+<legend>Status de conclusão do atendimento</legend>
+<label for="atendido">  Atendimento realizado </label>
+<input type="radio" name="status" id="atendido" value="atendido" checked />
+<label for="pendente"> Pendente de atendimento </label>
+<input type="radio" name="status" id="pendente" value="pendente" />
+</fieldset>
+</div>
+<button type="button" id="b" class="btn btn-primary">Cadastrar Atendimento</button>
 </form>
-<h2>Eventos que já foram atendidos </h2>
-/*
-<?php '
-$cx = new mysqli ("localhost","root","","funeraria");
-$eventos= $cx->query ("select * from eventos order by id");
-while ($evento = $eventos->fetch_assoc()){
-echo "<li>$evento[nome]</li>";
-}
-?>
-*/
-</body>
+
+    
+<!-- arquivos js importantes -->
+    <script src="js/jquery-3.4.1.js" ></script>
+    <script src="js/bootstrap.bundle.min.js" ></script>
+        <script src="js/functions.js" ></script>
+        <script>
+        
+        </script>
+  </body>
 </html>
